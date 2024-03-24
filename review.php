4e4ca4d48a465fd8e94d403ceb7b01be0998a571
ChisatoKano <129806087+kano0000@users.noreply.github.com>
2024-03-24 02:40:19 +0000

    5-10

--- review.php
+++ review.php
@@ -1,10 +1,12 @@
 <?php 
 class Review {
   private $menuName;
+  private $userName;
   private $body;
   
-  public function __construct($menuName, $body) {
+  public function __construct($menuName, $userName, $body) {
     $this->menuName = $manuName;
+    $this->userName = $userName;
     $this->body = $body;
   }
   
@@ -14,6 +16,14 @@ class Review {
   
   public function getbody() {
     return $this->body;
+  }
+  
+  public function getUser($users) {
+    foreach($users as $user) {
+      if($user->getName() == $this->userName) {
+        return $user;
+      }
+    }
   }
   
 }
--- show.php
+++ show.php
@@ -16,18 +16,20 @@ $menuReviews = $menu->getReviews($reviews);
   <link href='https://fonts.googleapis.com/css?family=Pacifico|Lato' rel='stylesheet' type='text/css'>
 </head>
 <body>
-  <div class="review-wrapper container">
-    <img src="<?php echo $menu->getImage() ?>" class="menu-item-image">
-    <h3 class="menu-item-name"><?php echo $menu->getName() ?></h3>
-
-    <?php if ($menu instanceof Drink): ?>
-      <p class="menu-item-type"><?php echo $menu->getType() ?></p>
-    <?php else: ?>
-      <?php for ($i = 0; $i < $menu->getSpiciness(); $i++): ?>
-        <img src="https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/chilli.png" class='icon-spiciness'>
-      <?php endfor ?>
-    <?php endif ?>
-    <p class="price">¥<?php echo $menu->getTaxIncludedPrice() ?></p>
+  <div class="review-wrapper">
+    <div class="review-menu-item">
+      <img src="<?php echo $menu->getImage() ?>" class="menu-item-image">
+      <h3 class="menu-item-name"><?php echo $menu->getName() ?></h3>
+  
+      <?php if ($menu instanceof Drink): ?>
+        <p class="menu-item-type"><?php echo $menu->getType() ?></p>
+      <?php else: ?>
+        <?php for ($i = 0; $i < $menu->getSpiciness(); $i++): ?>
+          <img src="https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/chilli.png" class='icon-spiciness'>
+        <?php endfor ?>
+      <?php endif ?>
+      <p class="price">¥<?php echo $menu->getTaxIncludedPrice() ?></p>
+    </div>
     
     <div class="review-list-wrapper">
       <div class="review-list">
@@ -36,10 +38,19 @@ $menuReviews = $menu->getReviews($reviews);
           <h4>レビュー一覧</h4>
         </div>
         <?php foreach($menuReviews as $review): ?>
-          <h3><?php echo $review->getMenuName() ?></h3>
-          <p><?php echo $review->getBody() ?></p>
+          <?php $user = $review->getUser($users) ?>
+          <div class="review-list-item">
+            <div class="review-user">
+              <?php if($user->getGender() == "male"):?>
+                <img src="https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/male.png" class='icon-user'>
+              <?php else: ?>
+                <img src="https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/female.png" class='icon-user'>
+              <?php endif ?>
+              <p><?php echo $user->getName() ?></p>
+            </div>
+            <p class="review-text"><?php echo $review->getBody() ?></p>
+          </div>
         <?php endforeach ?>
-        
       </div>
     </div>
     <a href="index.php">← メニュー一覧へ</a>
